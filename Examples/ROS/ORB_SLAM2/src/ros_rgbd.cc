f1ae5200fa45a1ae0c5e597efd3a803c2c83b573
Abrar Anwar <abraranwar@gmail.com>
2018-12-02 15:26:05 -0600

    orbslam publishes pose data

--- Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_rgbd.cc
@@ -29,11 +29,18 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
-
 #include<opencv2/core/core.hpp>
 
-#include"../../../include/System.h"
 
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <tf/transform_datatypes.h> 
+#include <tf/tf.h> 
+
+#include"../../../include/System.h"
+#include"../../../include/Converter.h"
 using namespace std;
 
 class ImageGrabber
@@ -49,6 +56,8 @@ public:
 
     ORB_SLAM2::System* mpSLAM;
 };
+
+ ros::Publisher pose_pub;
 
 int main(int argc, char **argv)
 {
@@ -69,6 +78,8 @@ int main(int argc, char **argv)
     igb.d = { new Detector() };
 
     ros::NodeHandle nh;
+
+    pose_pub = nh.advertise<geometry_msgs::PoseStamped>("camera/pose", 1000);
 
     message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, "/camera/rgb/image_raw", 1);
     message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, "camera/depth_registered/image_raw", 1);
@@ -125,7 +136,44 @@ void ImageGrabber::GrabRGBD(const sensor_msgs::ImageConstPtr& msgRGB,const senso
 
     //cout << "Number of detections: " << bBoxes.size() << "\n";
 
-    mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec(), bBoxes);
+
+    cv::Mat Tcw = mpSLAM->TrackRGBD(cv_ptrRGB->image,cv_ptrD->image,cv_ptrRGB->header.stamp.toSec(), bBoxes);
+
+    geometry_msgs::PoseStamped pose;
+    pose.header.stamp = ros::Time::now();
+    pose.header.frame_id = "map";
+
+    if(Tcw.empty()) {
+        pose.pose.position.x = 0;
+        pose.pose.position.y = 0;
+        pose.pose.position.z = 0;
+        pose.pose.orientation.x = 0;
+        pose.pose.orientation.y = 0;
+        pose.pose.orientation.z = 0;
+        pose.pose.orientation.w = 0;
+
+    }
+    else {
+    cv::Mat Rwc = Tcw.rowRange(0,3).colRange(0,3).t(); // Rotation information
+    cv::Mat twc = -Rwc*Tcw.rowRange(0,3).col(3); // translation information
+    vector<float> q = ORB_SLAM2::Converter::toQuaternion(Rwc);
+
+    tf::Transform new_transform;
+    new_transform.setOrigin(tf::Vector3(twc.at<float>(0, 0), twc.at<float>(0, 1), twc.at<float>(0, 2)));
+
+    tf::Quaternion quaternion(q[0], q[1], q[2], q[3]);
+    new_transform.setRotation(quaternion);
+
+    tf::poseTFToMsg(new_transform, pose.pose);
+
+    }
+    pose_pub.publish(pose);
+ 
+
+
+
+
+
 }
 
 
